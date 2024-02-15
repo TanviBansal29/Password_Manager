47761b67b4721dd2fc09e59e1cb5ef287167b64e
Tanvi Bansal <Tanvi.Bansal@watchguard.com>
2024-02-15 10:33:30 +0530

    .

--- src/controllers/admin_controller.py
+++ src/controllers/admin_controller.py
@@ -4,57 +4,59 @@ from utils.custom_error import DataAlreadyExists, DataNotFoundError
 
 
 class AdminController:
-    '''
-        Admin Controller class with admin methods
-    '''
-    
+    """
+    Admin Controller class with admin methods
+    """
+
     def __init__(self):
         self.admin_business = AdminBusiness()
 
     def register(self, register_data):
-        '''Method to create a new user'''
+        """Method to create a new user"""
 
         try:
             data = self.admin_business.fetch_username(register_data.username)
             if not data:
-                self.admin_business.create_user(register_data.username, register_data.password)
+                self.admin_business.create_user(
+                    register_data.username, register_data.password
+                )
                 return {"message": "User created successfully"}
-        except DataAlreadyExists:
-            return {"message" : "User"}
-        
+        except DataAlreadyExists as e:
+            raise HTTPException(status_code=409, detail=str(e))
+
     def view_all_user_data(self):
-        '''Method to view all users data'''
+        """Method to view all users data"""
 
         try:
             data = self.admin_business.view_all_user_data()
             return data
         except DataNotFoundError as e:
-            raise HTTPException(status_code = 404, detail = str(e))
-        
+            raise HTTPException(status_code=404, detail=str(e))
+
     def view_data_by_user(self, user_id):
-        '''Method to view all users data'''
+        """Method to view all users data"""
 
         try:
             data = self.admin_business.view_data_by_user(user_id)
             return data
         except DataNotFoundError as e:
-            raise HTTPException(status_code = 404, detail = str(e))
-        
+            raise HTTPException(status_code=404, detail=str(e))
+
     def view_user_data_by_website(self, website):
-        '''Method to view all users data'''
+        """Method to view all users data"""
 
         try:
             data = self.admin_business.view_user_data_by_website(website)
             return data
         except DataNotFoundError as e:
-            raise HTTPException(status_code = 404, detail = str(e))
-        
+            raise HTTPException(status_code=404, detail=str(e))
+
     def delete_user_data(self, user_id):
-        '''Method to view all users data'''
+        """Method to view all users data"""
 
         try:
             data = self.admin_business.view_data_by_user(user_id)
             if data:
                 return self.admin_business.delete_user(user_id)
         except DataNotFoundError as e:
-            raise HTTPException(status_code = 404, detail = str(e))
+            raise HTTPException(status_code=404, detail=str(e))
--- src/utils/tokens.py
+++ src/utils/tokens.py
@@ -7,43 +7,48 @@ from jose import ExpiredSignatureError, JWTError, jwt
 from typing import Annotated, Dict
 
 SECRET_KEY = "e0c5bd2b7eaece2c1f7a4e3b94a3d0e558fe6a0733a9af30d0d5f0c73d4e4d87"
-ALGORITHM = 'HS256'
+ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v1/login")
 
-def create_access_token(identity:str, fresh: bool, additional_claims: Dict):
-    '''Create access token'''
+
+def create_access_token(identity: str, fresh: bool, additional_claims: Dict):
+    """Create access token"""
 
     iat = datetime.utcnow()
     exp = iat + timedelta(minutes=15)
     payload = {
-        'fresh': fresh,
-        'iat': iat,
-        'type': 'access',
-        'sub': str(identity),
-        'exp': exp
+        "fresh": fresh,
+        "iat": iat,
+        "type": "access",
+        "sub": str(identity),
+        "exp": exp,
     }
     payload.update(additional_claims)
-    access_token = jwt.encode(payload, SECRET_KEY, ALGORITHM )
+    access_token = jwt.encode(payload, SECRET_KEY, ALGORITHM)
     return access_token
 
 
-def create_refresh_token(identity:str, additional_claims: Dict):
-    'Create a refresh token'
+def create_refresh_token(identity: str, additional_claims: Dict):
+    "Create a refresh token"
 
     iat = datetime.utcnow()
     exp = iat + timedelta(minutes=30)
     payload = {
-        'fresh': False,
-        'iat': iat,
-        'type': 'refresh',
-        'sub': str(identity),
-        'exp': exp
+        "fresh": False,
+        "iat": iat,
+        "type": "refresh",
+        "sub": str(identity),
+        "exp": exp,
     }
     payload.update(additional_claims)
     refresh_token = jwt.encode(payload, SECRET_KEY)
     return refresh_token
 
+
 def get_jwt(token: Annotated[str, Depends(oauth2_scheme)]):
-    '''Returns claims of a jwt token'''
-    payload = jwt.decode(token, SECRET_KEY)
-    return payload
+    """Returns claims of a jwt token"""
+    try:
+        payload = jwt.decode(token, SECRET_KEY)
+        return payload
+    except JWTError as e:
+        return {"error": str(e)}, 401
